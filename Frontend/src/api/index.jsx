5cff5d2a58b658bf7ceef1d0bad0679343043e2b
piyushbansal20 <115879708+piyushbansal20@users.noreply.github.com>
2025-07-31 15:02:39 +0530

    Update index.jsx

--- Frontend/src/api/index.jsx
+++ Frontend/src/api/index.jsx
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // Centralized API configuration using Axios.
 export const api = axios.create({
-    baseURL: 'https://data-dashboard-e3mq.onrender.com/api',
+    baseURL: 'https://localhost:3000/api',
 });
